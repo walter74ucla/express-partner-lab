08f56fa889be0b913f25a1034391399bfccc77d4
Walter Sylvester <walter_sylvester@msn.com>
2019-10-16 14:18:37 -0600

    created database

---
+++ models/sports.js
@@ -0,0 +1,19 @@
+const sports = [
+    {
+        name:'baseball',
+        color: 'red',
+        img: "https://i.imgur.com/0udv8ZT.png"
+    },
+    {
+        name:'football',
+        color: 'white',
+        img: "https://i.imgur.com/xm4Nx0Y.gif"
+    },
+    {
+        name:'basketball',
+        color: 'blue',
+        img: "https://i.imgur.com/Ox5WIoz.png"
+    }
+];
+
+module.exports = sports;
